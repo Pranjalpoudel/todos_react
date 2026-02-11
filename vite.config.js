99f9fc8d0fab34a18c2f636f3e3c1e0ba290fd44
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-02-11 17:05:55 +0545

    Update GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/todos_react/', // Base path for GitHub Pages deployment
+  base: '/todos_react/', // Base path for GitHub Pages project deployment
 })
